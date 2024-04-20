4c5e3f562de85ec98bd71a86660b3c1e6bcc1aeb
thakrisp <61070282+thakrisp@users.noreply.github.com>
2024-04-19 21:04:56 -0400

    resolving pnpm lock file err

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://thakrisp.github.io",
-  base: "/Resume",
+  base: "/resume",
 
   integrations: [tailwind()],
 });
